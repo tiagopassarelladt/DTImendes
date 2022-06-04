8d3411662c03dc776bd4449d1d979087b0dc85cf
tiagopassarelladt <62078607+tiagopassarella@users.noreply.github.com>
2022-06-04 08:46:58 -0300

    DTImentes

---
+++ Demo/DemoImendes.dpr
@@ -0,0 +1,16 @@
+program DemoImendes;
+
+uses
+  Vcl.Forms,
+  Unit4 in 'Unit4.pas' {Form4},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm4, Form4);
+  Application.Run;
+end.
